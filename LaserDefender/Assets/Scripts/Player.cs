d7ab2866e67a3ef40c9cb5bef8abe9bf890d4885
Donwaztok <igor.rmartins@outlook.com>
2021-04-27 00:47:01 -0300

    Added ship movement

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -1,11 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
+	// Params
+	[SerializeField] float moveSpeed = 10;
+	[SerializeField] float padding = 1;
+
+	// State
+	float xMin;
+	float xMax;
+	float yMin;
+	float yMax;
+
 	// Start is called before the first frame update
 	void Start() {
+		SetUpMoveBoundaries();
+	}
 
+	private void SetUpMoveBoundaries() {
+		Camera gameCamera = Camera.main;
+		xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + this.padding;
+		xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - this.padding;
+		yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + this.padding;
+		yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - this.padding;
 	}
 
 	// Update is called once per frame
@@ -14,8 +34,10 @@ public class Player : MonoBehaviour {
 	}
 
 	private void Move() {
-		float deltaX = Input.GetAxis("Horizontal");
-		float newXPos = transform.position.x + deltaX;
-		transform.position = new Vector2(newXPos, transform.position.y);
+		float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+		float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+		float newXPos = Mathf.Clamp(transform.position.x + deltaX, this.xMin, this.xMax);
+		float newYPos = Mathf.Clamp(transform.position.y + deltaY, this.yMin, this.yMax);
+		transform.position = new Vector2(newXPos, newYPos);
 	}
 }
